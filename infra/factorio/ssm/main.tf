0184ac68dacf738ebe3bb041abd534cd58545c6d
Mateusz Biel <matbiel@gmail.com>
2021-09-05 19:53:05 +0200

    Add saves restore document

--- infra/factorio/ssm/main.tf
+++ infra/factorio/ssm/main.tf
@@ -6,3 +6,13 @@ resource "aws_ssm_document" "saves_backup" {
   })
   document_format = "YAML"
 }
+
+
+resource "aws_ssm_document" "saves_restore" {
+  name = "factorio_saves_restore"
+  document_type = "Command"
+  content = templatefile("${path.module}/restore_saves_from_s3.yaml.tmpl", {
+    "bucket_name": var.saves_backup_bucket_id
+  })
+  document_format = "YAML"
+}
