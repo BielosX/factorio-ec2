b208fa834dc86d61b78ab047d7765d00cbd74df8
Mateusz Biel <matbiel@gmail.com>
2022-02-26 10:20:28 +0100

    Add remove_images command

--- run.sh
+++ run.sh
@@ -44,6 +44,23 @@ restore_saves() {
   aws ssm send-command --document-name factorio_saves_restore --targets Key=tag:Name,Values=factorio-server
 }
 
+remove_images() {
+  images=$(aws ec2 describe-images --filters "Name=tag:Name,Values=factorio-server-image")
+  for k in $(echo "$images" | jq -r '.Images | keys | .[]'); do
+    image=$(echo "$images" | jq -r ".Images[$k]")
+    image_id=$(echo "$image" | jq -r '.ImageId')
+    mapping_keys=$(echo "$image" | jq -r '.BlockDeviceMappings | keys | .[]')
+    snapshot_ids=$(echo "$image" | jq -r '.BlockDeviceMappings | map(.Ebs.SnapshotId)')
+    echo "Deleting AMI $image_id"
+    aws ec2 deregister-image --image-id "$image_id"
+    for id in $mapping_keys; do
+      snapshot_id=$(echo "$snapshot_ids" | jq -r ".[$id]")
+      echo "Deleting snapshot $snapshot_id"
+      aws ec2 delete-snapshot --snapshot-id "$snapshot_id"
+    done
+  done
+}
+
 case "$1" in
   "infra") deploy_infra ;;
   "destroy_infra") destroy_infra ;;
@@ -51,5 +68,6 @@ case "$1" in
   "vagrant_up") vagrant_up ;;
   "vagrant_destroy") vagrant_destroy ;;
   "restore_saves") restore_saves ;;
-  *) echo "Actions: infra/destroy_infra/image/vagrant_up/vagrant_destroy/restore_saves" ;;
+  "remove_images") remove_images ;;
+  *) echo "Actions: infra/destroy_infra/image/vagrant_up/vagrant_destroy/restore_saves/remove_images" ;;
 esac
